4b219d47f3549c2bc8d7c66c742547db0b41f731
arapocket <arakazaryan@me.com>
2019-01-27 15:33:25 -0800

    quick save

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const sql = require('mssql')
 
 
-sql.connect('mssql://Administrator:@localhost/mobss').then(res => {
+sql.connect('mssql://EC2AMAZ-86TAJ27\Administrator:@localhost/mobss').then(res => {
     sql.query`select * from mytable where id = ${value}
     `
     console.log(res);
