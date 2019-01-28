d84f9ae5d25ea810f56be0db2f084ff8a039d0db
arapocket <arakazaryan@me.com>
2019-01-27 16:13:27 -0800

    quick save

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const sql = require('mssql')
 const config = {
     user: 'Administrator',
     password: '!',
-    server: 'EC2AMAZ-86TAJ27', // You can use 'localhost\\instance' to connect to named instance
+    server: 'localhost\\EC2AMAZ-86TAJ27', // You can use 'localhost\\instance' to connect to named instance
     database: 'mobss',
 
     options: {
